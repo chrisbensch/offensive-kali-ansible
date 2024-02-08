f53148ce0e1ef355758ba62e8d9f06e14a354f16
Chris Bensch <chris.bensch@gmail.com>
2024-02-08 13:20:45 -0800

    Install shell scripts

---
+++ preinstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# For whatever reason, I cannot seem to get ansible to correctly recognize
+# the github3.py module if installed during a playbook.  The below should
+# be temporary.
+
+pip3 install github3.py
+
+# Qterminal seems to overwrite it's settings when closed so I do the install
+# from Xterm to ensure there are no issues.
+sudo apt update && sudo apt -y install ansible xterm
+
+# This installs the two external roles I need.
+ansible-galaxy install -r requirements.yml
+
