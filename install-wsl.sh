dc8430a55cdedd348eda9844c0b40c685cb6d6d5
Chris Bensch <chris.bensch@gmail.com>
2024-03-22 14:08:14 -0700

    More wsl role work

--- install-wsl.sh
+++ install-wsl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # git pulls the most current changes and begins playbook
-clear && git pull && ansible-playbook -i ansible/local.ini site.yml -K
+clear && git pull && ansible-playbook -i ansible/local.ini site-wsl.yml -K
---
+++ preinstall-wsl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# For whatever reason, I cannot seem to get ansible to correctly recognize
+# the github3.py module if installed during a playbook.  The below should
+# be temporary.
+
+sudo apt install python3 python3-pip
+
+sudo pip3 install github3.py
+
+# Qterminal seems to overwrite it's settings when closed so I do the install
+# from Xterm to ensure there are no issues.
+sudo apt update && sudo apt -y install ansible-core
+
+# This installs the two external roles I need.
+ansible-galaxy install -r requirements-wsl.yml
+
