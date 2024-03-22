59b146d827c304a057d253a5f398c031aa10e82e
Chris Bensch <chris.bensch@gmail.com>
2024-03-22 13:26:23 -0700

    update ansible package name to ansible-core

--- preinstall.sh
+++ preinstall.sh
@@ -8,7 +8,7 @@ sudo pip3 install github3.py
 
 # Qterminal seems to overwrite it's settings when closed so I do the install
 # from Xterm to ensure there are no issues.
-sudo apt update && sudo apt -y install ansible xterm
+sudo apt update && sudo apt -y install ansible-core xterm
 
 # This installs the two external roles I need.
 ansible-galaxy install -r requirements.yml
