9c31865378891f8ef07d9994088109d05e1f8557
Chris Bensch <chris.bensch@gmail.com>
2024-11-04 13:25:54 -0800

    Marker

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+#python3 -m venv ansible_env
+#source ansible_env/bin/activate
+#pip install ansible github3.py
+
 pip install github3.py
 # git pulls the most current changes and begins playbook
 clear && git pull && ansible-playbook -i ansible/local.ini site.yml -K
