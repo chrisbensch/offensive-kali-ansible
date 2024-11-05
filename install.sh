bbf1bd54115090ef6f55d97000a4da8e2324ef83
Chris Bensch <chris.bensch@gmail.com>
2024-11-05 10:07:43 -0800

    No github3.py

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@
 #source ansible_env/bin/activate
 #pip install ansible github3.py
 
-pip install github3.py
+#pip install github3.py
 # git pulls the most current changes and begins playbook
 clear && git pull && ansible-playbook -i ansible/local.ini site.yml -K
