1ab8efae7f0a985325ea1c96e3f141ca7bd8dcf6
Chris Bensch <chris.bensch@gmail.com>
2024-11-11 08:44:45 -0800

    single script test

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 
-#python3 -m venv ansible_env
-#source ansible_env/bin/activate
-#pip install ansible github3.py
+# Qterminal seems to overwrite it's settings when closed so I do the install
+# from Xterm to ensure there are no issues.
+# but not anymore?
 
-#pip install github3.py
+sudo apt update && sudo apt -y install ansible-core #xterm
+
+# This installs the two external roles I need.
+ansible-galaxy install -r requirements.yml
 # git pulls the most current changes and begins playbook
 clear && git pull && ansible-playbook -i ansible/local.ini site.yml -K
