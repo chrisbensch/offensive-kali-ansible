ad2d93162da08700915f893de30af9caba6fb09d
Chris Bensch <chris.bensch@gmail.com>
2024-11-15 14:40:52 -0800

    ssshhhhh

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 
 # Qterminal seems to overwrite it's settings when closed so I do the install
 # from Xterm to ensure there are no issues.
-
-sudo apt update && sudo apt -y install ansible-core xterm
+sudo apt -qq update && sudo apt -y -qq install ansible-core xterm
 
 # This installs the two external roles I need.
 ansible-galaxy install -r requirements.yml
+
 # git pulls the most current changes and begins playbook
 clear && git pull && ansible-playbook -i ansible/local.ini site.yml
